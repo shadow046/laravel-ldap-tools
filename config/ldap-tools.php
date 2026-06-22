da761d6e462e5a020083914960e10b967ed9d6ba
shadow046 <53925724+shadow046@users.noreply.github.com>
2026-06-22 09:00:35 +0800

    Add LDAP tools config

---
+++ config/ldap-tools.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | LDAP Connection
+    |--------------------------------------------------------------------------
+    |
+    | Use LDAP_SSL=true for LDAPS, usually port 636. Use LDAP_TLS=true only
+    | when your server expects StartTLS, usually over port 389.
+    |
+    */
+
+    'host' => env('LDAP_HOST'),
+    'port' => (int) env('LDAP_PORT', 389),
+    'base_dn' => env('LDAP_BASE_DN'),
+    'username' => env('LDAP_USERNAME'),
+    'password' => env('LDAP_PASSWORD'),
+    'ssl' => filter_var(env('LDAP_SSL', false), FILTER_VALIDATE_BOOLEAN),
+    'tls' => filter_var(env('LDAP_TLS', false), FILTER_VALIDATE_BOOLEAN),
+    'timeout' => (int) env('LDAP_TIMEOUT', 5),
+    'logging' => filter_var(env('LDAP_LOGGING', false), FILTER_VALIDATE_BOOLEAN),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Profile Fields
+    |--------------------------------------------------------------------------
+    |
+    | These attributes are read from LDAP and normalized by the service.
+    | No password fields are read or printed by the package commands.
+    |
+    */
+
+    'attributes' => [
+        'dn',
+        'samaccountname',
+        'employeeid',
+        'employeenumber',
+        'mail',
+        'userprincipalname',
+        'cn',
+        'displayname',
+        'givenname',
+        'sn',
+        'department',
+        'title',
+        'company',
+        'telephonenumber',
+        'mobile',
+        'memberof',
+    ],
+];
